e0734a86e57f445f91b70cda80ba5c6d5df6d41e
STUDYGROUPHUB <studygroup207@gmail.com>
2026-03-10 14:14:51 +0530

    Update bot credentials in main.pyCGAN

--- main.py
+++ main.py
@@ -41,9 +41,9 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 
 # Bot credentials from environment variables (Render compatible)
-API_ID = int(os.environ.get("API_ID", 24473318))
-API_HASH = os.environ.get("API_HASH", "e7dd0576c5ac0ff8f90971d6bb04c8f5")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0")
+API_ID = int(os.environ.get("API_ID", 30828401))
+API_HASH = os.environ.get("API_HASH", "c373c4d5d7f1184b175ae73398c0e576")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8698774162:AAE5KxtR0vVj2g7NEnJHimUpbjwLK7COo8g")
 
 # Initialize Bot Globally (IMPORTANT FIX)
 bot = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
