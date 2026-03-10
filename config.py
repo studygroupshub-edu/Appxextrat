db69ef73cbcedd1ba2c966a6eee44c9c9eabb728
STUDYGROUPHUB <studygroup207@gmail.com>
2026-03-10 14:08:46 +0530

    Change API credentials and auth users in config.pychange
    Updated API credentials and authorized users.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = "30828401"
+api_hash = "c373c4d5d7f1184b175ae73398c0e576"
+bot_token = "8698774162:AAE5KxtR0vVj2g7NEnJHimUpbjwLK7COo8g"
+auth_users = [8535008486]
